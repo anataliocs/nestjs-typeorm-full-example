ffec602f93f1153c0bc7043c59e0d67e5d685a4c
Chris Anatalio <anataliocs@gmail.com>
2025-10-14 17:47:43 +0800

    Add endpoint examples

--- src/stablecoins/stablecoins.controller.ts
+++ src/stablecoins/stablecoins.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { StablecoinsService } from './stablecoins.service';
+import { CustomerDto } from './dto/customer.dto';
 
 @Controller('/v1/stablecoins')
 export class StablecoinsController {
   constructor(private readonly stablecoinsService: StablecoinsService) {}
 
   @Get('/:id/customers')
-  getCustomers(@Param('id') id: string): string {
+  @Header('Cache-Control', 'no-store')
+  @HttpCode(200)
+  getCustomersById(@Param('id') id: string): string {
     // Call Service layer to get customers
-    return this.stablecoinsService.getCustomers(id);
+    return this.stablecoinsService.getCustomersById(id);
+  }
+
+  @Get('/:name/customers')
+  @Header('Cache-Control', 'no-store')
+  @HttpCode(200)
+  getCustomersByName(@Param('name') name: string): string {
+    // Call Service layer to get customers
+    return this.stablecoinsService.getCustomersByName(name);
+  }
+
+  @Post('/customers')
+  @Header('Cache-Control', 'no-store')
+  @HttpCode(201)
+  createCustomer(@Body() customer: CustomerDto): string {
+    console.log(customer);
+    return 'Customer created';
   }
 }
--- src/stablecoins/stablecoins.service.ts
+++ src/stablecoins/stablecoins.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StablecoinsService {
-  getCustomers(id: string): string {
+  getCustomersById(id: string): string {
     console.log(id);
 
     return 'Customers';
+  }
+  getCustomersByName(name: string): string {
+    console.log(name);
+
+    return 'Customers by Name';
   }
 }
