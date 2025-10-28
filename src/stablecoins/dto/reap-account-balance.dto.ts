bf8b8fe8f8a2882598272faf74c5acd2d38beeff
Chris Anatalio <anataliocs@gmail.com>
2025-10-28 20:56:48 +0800

    Typesafe api request

---
+++ src/stablecoins/dto/reap-account-balance.dto.ts
@@ -0,0 +1,11 @@
+/*
+ * Classes are ES6 standard and remain intact as real entities in the compiled JavaScript.
+ * TypeScript interfaces are removed during transpilation.
+ * Nest can't reference them at runtime, and Pipes rely on
+ * having access to the metatype of variables at runtime
+ */
+
+export class ReapAccountBalanceDto {
+  availableBalance: string;
+  availableToAllocate: string;
+}
--- src/stablecoins/stablecoins.service.ts
+++ src/stablecoins/stablecoins.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CustomerDto } from './dto/customer.dto';
 import { HttpService } from '@nestjs/axios';
+import { ReapAccountBalanceDto } from './dto/reap-account-balance.dto';
 
 @Injectable()
 export class StablecoinsService {
+  private readonly logger = new Logger(StablecoinsService.name);
+
   constructor(private readonly httpService: HttpService) {}
 
   private accountBalanceResource: string = 'account/balance';
@@ -11,9 +14,13 @@ export class StablecoinsService {
   getCustomersById(id: string): CustomerDto {
     console.log(id);
 
-    this.httpService.get(this.accountBalanceResource).subscribe((res) => {
-      console.log(res.data);
-    });
+    this.httpService
+      .get<ReapAccountBalanceDto>(this.accountBalanceResource)
+      .subscribe((res) => {
+        console.log(res.data);
+        const balance: ReapAccountBalanceDto = res.data;
+        this.logger.log(`Available Balance ${balance.availableBalance}: `);
+      });
 
     return {
       name: 'John',
