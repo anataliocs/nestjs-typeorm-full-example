5fcbb87bfb3af2bc7405c774c6f51d59fd6ce83f
Chris Anatalio <anataliocs@gmail.com>
2025-11-10 21:32:14 +0800

    Fix tests

--- src/stablecoins/stablecoins.service.spec.ts
+++ src/stablecoins/stablecoins.service.spec.ts
@@ -2,21 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StablecoinsService } from './stablecoins.service';
 import { HttpService } from '@nestjs/axios';
 import { AccountBalanceDto } from './dto/reap/account-balance.dto';
+import { of } from 'rxjs';
+import { AxiosResponse } from 'axios';
+
+const mockAccountDataDto = {
+  availableBalance: '100',
+  availableToAllocate: '1000',
+} as AccountBalanceDto;
+
+const mockAxiosResponse = {
+  data: mockAccountDataDto,
+  status: 200,
+  statusText: 'OK',
+  headers: {},
+  config: {},
+} as AxiosResponse<AccountBalanceDto, any, object>;
 
 describe('StablecoinsService', () => {
   let service: StablecoinsService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StablecoinsService],
+      providers: [
+        StablecoinsService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(() => of(mockAxiosResponse)),
+            pipe: jest.fn(() => of(mockAxiosResponse)),
+          },
+        },
+      ],
     })
       .useMocker((token) => {
-        if (token === HttpService) {
+        if (token === null) {
           return jest.fn();
         }
       })
       .compile();
 
+    httpService = module.get<HttpService>(HttpService);
     service = module.get<StablecoinsService>(StablecoinsService);
   });
 
@@ -28,7 +54,8 @@ describe('StablecoinsService', () => {
     const balance: AccountBalanceDto = await service.getMasterAccountBalance();
     console.log(balance);
     expect(balance).toBeDefined();
-    expect(balance).toHaveProperty('name', 'John');
-    expect(balance).toHaveProperty('id', '123');
+    expect(httpService['get']).toHaveBeenCalledWith('account/balance');
+    expect(balance).toHaveProperty('availableBalance', '100');
+    expect(balance).toHaveProperty('availableToAllocate', '1000');
   });
 });
