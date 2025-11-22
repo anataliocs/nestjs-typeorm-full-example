3822cc06f3df8f27523f76e858b7d8803a84584b
Chris Anatalio <anataliocs@gmail.com>
2025-11-23 04:24:03 +0800

    Peaq SDK module lifecycle handling

--- src/peaqsdk/peaq.sdk.service.ts
+++ src/peaqsdk/peaq.sdk.service.ts
@@ -8,35 +8,53 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { Sdk } from '@peaq-network/sdk';
 
+const NOT_CONNECTED = 'Not Connected';
+const CONNECTED = 'Connected';
+
+type PeaqSdk = Sdk;
 @Injectable()
 export class PeaqSdkService implements OnApplicationShutdown, OnModuleInit {
   private readonly logger = new Logger(PeaqSdkService.name);
 
   get rpcServer() {
+    if (!this._rpcServer || this._rpcServerStatus === NOT_CONNECTED)
+      throw new Error(`Peaq SDK Rpc Server is not initialized.`);
     return this._rpcServer;
   }
 
-  private readonly _rpcServer: Promise<Sdk>;
+  private readonly _rpcServerProvider: () => Promise<PeaqSdk>;
+  private _rpcServer: PeaqSdk;
+  private _rpcServerStatus: string = NOT_CONNECTED;
+  private readonly _rpcServerUrl: string;
 
   // https://docs.peaq.xyz/sdk-reference/javascript/create-instance
   constructor(
     private configService: ConfigService,
     @Inject('CONFIG_OPTIONS') private options: Record<string, string>,
   ) {
-    this._rpcServer = Sdk.createInstance({
-      baseUrl:
-        this.configService.get<string>('PEAQ_RPC_SERVER_URL') ||
-        options.rpcServerUrl,
-      chainType: Sdk.ChainType.EVM,
-    });
+    this._rpcServerUrl =
+      this.configService.get<string>('PEAQ_RPC_SERVER_URL') ||
+      options.rpcServerUrl;
+    this._rpcServerProvider = async () =>
+      await Sdk.createInstance({
+        baseUrl: this._rpcServerUrl,
+        chainType: Sdk.ChainType.EVM,
+      });
   }
 
-  onModuleInit() {
-    this.logger.log(`Peaq SDK Rpc Server Status: - Rpc Server URL:`);
+  async onModuleInit() {
+    this._rpcServer = await this._rpcServerProvider();
+    await this._rpcServer.connect();
+    this._rpcServerStatus = CONNECTED;
+    this.logger.log(
+      `Peaq SDK Rpc Server Status: ${this._rpcServerStatus} - Rpc Server URL: ${this._rpcServerUrl}`,
+    );
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  onApplicationShutdown(_signal?: string) {
+  async onApplicationShutdown(_signal?: string) {
+    await this._rpcServer?.disconnect();
+    this._rpcServerStatus = NOT_CONNECTED;
     this.logger.log(`Closing Peaq SDK Rpc Server Connection`);
   }
 }
--- src/peaqsdk/peaqSdkModule.ts
+++ src/peaqsdk/peaqSdkModule.ts
@@ -2,6 +2,7 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PeaqSdkService } from './peaq.sdk.service';
 
+// https://docs.nestjs.com/fundamentals/dynamic-modules
 @Module({ imports: [ConfigModule] })
 export class PeaqSdkModule {
   static register(options: Record<string, string>): DynamicModule {
