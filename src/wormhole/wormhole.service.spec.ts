b8dbb7c5de1ab9e0eee6d2f55d553ac85716fb1b
Chris Anatalio <anataliocs@gmail.com>
2025-11-26 06:42:57 +0800

    Update tests and setup code coverage

--- src/wormhole/wormhole.service.spec.ts
+++ src/wormhole/wormhole.service.spec.ts
@@ -5,7 +5,8 @@ import { WormholeSdkService } from '../wormholesdk/wormhole.sdk.service';
 import { WormholeSdkConfig } from '../wormholesdk/wormholeSdkConfig';
 import evm from '@wormhole-foundation/sdk/evm';
 import solana from '@wormhole-foundation/sdk/solana';
-import { Wormhole } from '@wormhole-foundation/sdk';
+import { ChainConfig, ChainContext, Wormhole } from '@wormhole-foundation/sdk';
+import { ChainConfigDto } from './dto/chain-config.dto';
 
 describe('WormholeService', () => {
   let service: WormholeService;
@@ -31,6 +32,10 @@ describe('WormholeService', () => {
     configService = module.get<ConfigService>(ConfigService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(wormholeSdkService).toBeDefined();
@@ -38,24 +43,71 @@ describe('WormholeService', () => {
   });
 
   it('serverStatus() response should be defined', () => {
-    jest
+    const wormholeSdkMock = jest
       .spyOn(wormholeSdkService, 'wormholeServerStatus', 'get')
       .mockReturnValue('Connected');
 
     const status: string = service.serverStatus();
     expect(status).toBeDefined();
+    expect(wormholeSdkMock).toHaveBeenCalledTimes(1);
     expect(status).toBe('Connected');
   });
 
   it('network() response should be defined', () => {
     const wormholeServer: Wormhole<'Testnet'> = new Wormhole('Testnet', []);
-    jest.spyOn(wormholeServer, 'network', 'get').mockReturnValue('Testnet');
-    jest
+    const wormholeServerMock = jest
+      .spyOn(wormholeServer, 'network', 'get')
+      .mockReturnValue('Testnet');
+    const wormholeSdkMock = jest
       .spyOn(wormholeSdkService, 'wormholeServer', 'get')
       .mockReturnValue(wormholeServer);
 
     const status: string = service.network();
     expect(status).toBeDefined();
+    expect(wormholeServerMock).toHaveBeenCalledTimes(1);
+    expect(wormholeSdkMock).toHaveBeenCalledTimes(1);
     expect(status).toBe('Testnet');
+  });
+
+  it('getChainContext() response should be defined', () => {
+    const wormholeServer: Wormhole<'Testnet'> = new Wormhole('Testnet', []);
+    const wormholeServerMock = jest
+      .spyOn(wormholeServer, 'getChain')
+      .mockReturnValue({
+        config: {
+          key: 'Ethereum',
+          platform: 'Evm',
+          network: 'Testnet',
+          chainId: 2,
+          finalityThreshold: 72,
+          blockTime: 15000,
+          nativeTokenDecimals: 18,
+          rpc: '',
+          contracts: {
+            coreBridge: '0x706abc4E45D419950511e474C7B9Ed348A4a716c',
+          },
+          wrappedNative: { symbol: 'WETH' },
+        } as ChainConfig<'Testnet', 'Ethereum'>,
+      } as ChainContext<'Testnet'>);
+    const wormholeSdkMock = jest
+      .spyOn(wormholeSdkService, 'wormholeServer', 'get')
+      .mockReturnValue(wormholeServer);
+
+    const chainConfig: ChainConfigDto = service.getChainContext('Ethereum');
+    expect(chainConfig).toBeDefined();
+    expect(wormholeServerMock).toHaveBeenCalledTimes(1);
+    expect(wormholeSdkMock).toHaveBeenCalledTimes(1);
+    expect(chainConfig).toHaveProperty('chainId', 2);
+    expect(chainConfig).toHaveProperty('rpc', '');
+    expect(chainConfig).toHaveProperty('platform', 'Evm');
+    expect(chainConfig).toHaveProperty('network', 'Testnet');
+    expect(chainConfig).toHaveProperty('blockTime', 15000);
+    expect(chainConfig).toHaveProperty('finality', 72);
+    expect(chainConfig).toHaveProperty('nativeTokenDecimals', 18);
+    expect(chainConfig).toHaveProperty('wrappedNativeSymbol', 'WETH');
+    expect(chainConfig).toHaveProperty(
+      'coreBridgeAddress',
+      '0x706abc4E45D419950511e474C7B9Ed348A4a716c',
+    );
   });
 });
