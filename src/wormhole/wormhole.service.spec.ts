315fba57fefd6d45fde11d5c1387488c4231b603
Chris Anatalio <anataliocs@gmail.com>
2025-11-26 04:40:18 +0800

    Add unit test for wormhole svc

--- src/wormhole/wormhole.service.spec.ts
+++ src/wormhole/wormhole.service.spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { WormholeService } from './wormhole.service';
 import { ConfigService } from '@nestjs/config';
 import { WormholeSdkService } from '../wormholesdk/wormhole.sdk.service';
+import { WormholeSdkConfig } from '../wormholesdk/wormholeSdkConfig';
+import evm from '@wormhole-foundation/sdk/evm';
+import solana from '@wormhole-foundation/sdk/solana';
+import { Wormhole } from '@wormhole-foundation/sdk';
 
 describe('WormholeService', () => {
   let service: WormholeService;
@@ -14,7 +18,10 @@ describe('WormholeService', () => {
     })
       .useMocker((token) => {
         if (token === 'CONFIG_OPTIONS') {
-          return jest.fn();
+          return jest.fn().mockReturnValue({
+            wormholeNetwork: 'Testnet',
+            platformArray: [evm, solana],
+          } as WormholeSdkConfig);
         }
       })
       .compile();
@@ -28,5 +35,27 @@ describe('WormholeService', () => {
     expect(service).toBeDefined();
     expect(wormholeSdkService).toBeDefined();
     expect(configService).toBeDefined();
+  });
+
+  it('serverStatus() response should be defined', () => {
+    jest
+      .spyOn(wormholeSdkService, 'wormholeServerStatus', 'get')
+      .mockReturnValue('Connected');
+
+    const status: string = service.serverStatus();
+    expect(status).toBeDefined();
+    expect(status).toBe('Connected');
+  });
+
+  it('network() response should be defined', () => {
+    const wormholeServer: Wormhole<'Testnet'> = new Wormhole('Testnet', []);
+    jest.spyOn(wormholeServer, 'network', 'get').mockReturnValue('Testnet');
+    jest
+      .spyOn(wormholeSdkService, 'wormholeServer', 'get')
+      .mockReturnValue(wormholeServer);
+
+    const status: string = service.network();
+    expect(status).toBeDefined();
+    expect(status).toBe('Testnet');
   });
 });
